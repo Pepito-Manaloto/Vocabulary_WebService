d5b6f531e498dd1816bee72fa3f09725fde77507
Aaron John Asuncion <ajlasuncion@chikka.com>
2015-05-29 16:03:56 +0800

    Added connection to database: - executing of query not yet working. - parsing of query result not yet implemented.

--- routes/vocabulary.js
+++ routes/vocabulary.js
@@ -1,8 +1,12 @@
 const AUTH_KEY = "aaron";
+const DB_HOST = "localhost";
+const DB_USER = "root";
+const DB_PASS = "root";
+const DB_SCHEMA = "my_vocabulary";
 
 //(req.params) Checks route params, ex: /user/:id 
 //(req.query) Checks query string params, ex: ?id=12 Checks urlencoded body params 
-function getFilterDate(request, response)
+exports.getFilterDate = function(request, response)
 {
     var crypto = require("crypto");
     var md5sum = crypto.createHash("md5");
@@ -14,7 +18,7 @@ function getFilterDate(request, response)
         {
             if(auth == md5sum.update(AUTH_KEY).digest("hex"))
             {
-                var lastUpdated = request.query("last_updated");
+                var lastUpdated = request.query["last_updated"];
                 if(!lastUpdated || lastUpdated.length == 0) // If last_updated is not given, then set earliest date
                 {
                     lastUpdated = "1950-01-01";
@@ -22,8 +26,9 @@ function getFilterDate(request, response)
 
                 response.setHeader("Content-Type", "application/json");
                 response.status(200);
-                
-                var result = {"response_text": "success"};
+
+                var result = getVocabularies(lastUpdated);
+
                 response.send(result);
             }
             else
@@ -40,10 +45,42 @@ function getFilterDate(request, response)
     }
     catch(err)
     {
-        console.log(err);
+        console.log(err + "\n" + err.stack);
         response.status(500);
         response.send("Internal Server Error.");
     }
+};
+
+function getVocabularies(lastUpdated)
+{
+    var mysql = require("mysql");
+    var conn =  mysql.createConnection(
+        {
+            host:     DB_HOST,
+            user:     DB_USER,
+            password: DB_PASS,
+            database: DB_SCHEMA,
+        });
+    
+    conn.connect(function(err)
+        {
+            if(err)
+            {
+                console.log("Error in connecting to database.\n%s.\n", err);
+            }
+        });
+    
+    conn.query("CALL Get_Vocabularies(" + lastUpdated + ", @recently_added_count); SELECT @recently_added_count;", processGetVocabularies);
+    
+    return '{"response": "success"}';
+}
+
+function processGetVocabularies(err, results, fields)
+{
+    console.log(results);
+    console.log(fields);
+    console.log(printObject(results));
+    console.log(printObject(fields));
 }
 
 function printObject(obj)
@@ -51,7 +88,7 @@ function printObject(obj)
     var out = '';
     for (var key in obj)
     {
-    out += key + ': ' + obj[key] + '\n';
+        out += key + ': ' + obj[key] + '\n';
     }
     return out;
 }
